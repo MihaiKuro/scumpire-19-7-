3c532b6388b0e1f79610f26b5f70d439e4aed502
MihaiKuro <stanciu.c.mihai22@stud.rau.ro>
2024-01-23 00:02:44 +0200

    tva 60-->55%

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function calculeazaPretFinal(scumpireType) {
     let pretInitial = parseFloat(document.getElementById("pret_initial_" + scumpireType).value);
 
     // Scumpire 1: 65%
-    let scumpirePrima = 1.65;
+    let scumpirePrima = 1.55;
     let pretDupaPrimaScumpire = pretInitial * scumpirePrima;
 
     // Scumpire 2: 19% sau 7%
@@ -16,19 +16,19 @@ function calculeazaPretFinal(scumpireType) {
     let rezultatDiv = document.getElementById("rezultat_" + scumpireType);
     let istoricList = document.getElementById("istoric-list_" + scumpireType);
 
-    rezultatDiv.innerHTML = "Pretul final este: " + pretFinal.toFixed(3);
+    rezultatDiv.innerHTML = "Pretul final este: " + pretFinal.toFixed(4);
 
     // Adaugă în istoric cu inversarea ordinii
     let listItem = document.createElement("li");
-    listItem.appendChild(document.createTextNode("Calcul " + (scumpireType === '19' ? arr19.length +1 : arr7.length +1) + ": " + pretFinal.toFixed(3)));
+    listItem.appendChild(document.createTextNode("Calcul " + (scumpireType === '19' ? arr19.length +1 : arr7.length +1) + ": " + pretFinal.toFixed(4)));
 
     istoricList.insertBefore(listItem, istoricList.firstChild);
 
     // Adaugă la istoric
     if (scumpireType === '19') {
-        arr19.unshift(pretFinal.toFixed(3));
+        arr19.unshift(pretFinal.toFixed(4));
     } else if (scumpireType === '7') {
-        arr7.unshift(pretFinal.toFixed(3));
+        arr7.unshift(pretFinal.toFixed(4));
     }
     
 }
